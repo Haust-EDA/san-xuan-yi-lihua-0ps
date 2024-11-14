2c70702041a7d795323a996baee9a5aafa5fbcd2
lihua-0ps <2100244776@qq.com>
2024-11-14 13:02:32 +0800

    first commit

--- main.c
+++ main.c
@@ -1,1 +1,39 @@
 //请在下方输入你的代码：
+//以下是正三角
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	int m, p = 1;
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			m = i * j;
+			printf("%d*%d=%d ", i, j, m);
+			p++;
+		}
+		if (p = j)printf("\n");
+
+	}
+	return 0;
+}
+//以下是倒三角
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	int m, p = 1;
+	for (i = 9; i >= 1; i--)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			m = i * j;
+			printf("%d*%d=%d ", i, j, m);
+			p++;
+		}
+		if (p = j)printf("\n");
+
+	}
+	return 0;
+}
